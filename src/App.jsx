97f0dd0c23ebe853ee390e2f7a661e94b389d946
Himaaya <himaayakavin@gmail.com>
2025-12-23 10:57:35 +0530

    tailwind & counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import DataDisplay from'./components/Dashboard.jsx'
+import Dashboard from './components/Dashboard.jsx'
+import Counter from './components/Counter.jsx'
 
 function App(){
+    const[name,setName]=useState("Himaaya")
+    const[occ,setOcc]=useState("developer")
+    function nameChange(){
+        setName("maaya")
+        console.log(name)
+    }
     return(
         <>
-            <h1>HELLO EVERYONE</h1>
-            <h2>WELCOME TO REACT JS</h2>
+            <Dashboard name={name}occ={occ}nameChange={nameChange}/>
+           <Counter/> 
         </>
-        
     )
 }
 
---
+++ src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import React,{useState} from"react"
+export default function Counter(){
+    const[count,setCount]=useState(0)
+    function increment(){
+        setCount(count+1)
+    }
+    return(
+        <>
+            <h1>{count}</h1>
+            <button onClick={increment}>+</button>
+        </>
+    )
+}
